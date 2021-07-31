e8a16ea8e4f9ab6b680d5a0fb4785b21cfedcc94
Yashu Garg <garg.y2001@gmail.com>
2021-07-31 14:05:10 +0530

    Initial Commit

---
+++ bot.py
@@ -0,0 +1,19 @@
+import discord
+import random
+import os
+import youtube_dl
+import wikipedia
+from discord.ext import commands, tasks
+from discord.utils import get
+from itertools import cycle
+import sqlite3
+from datetime import datetime
+import time
+import database
+import json
+import re
+from discord.ext import commands
+from database import *
+import pkg_resources
+import asyncio
+import aiml
